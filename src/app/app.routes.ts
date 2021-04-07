1b4cdb2d201290ccbd146b3028b677e48ad4da7a
raquelcfatima <raquel.fatima@webradar.com>
2021-04-06 21:22:18 -0300

    page movie list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,22 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from '@src/app/pages/about/about.component';
+import { MovieListComponent } from '@src/app/pages/movie-list/movie-list.component';
 
 
 export const routes: Routes = [
   {
-      path: '',
-      redirectTo: '/about',
-      pathMatch: 'full',
+    path: '',
+    redirectTo: '/movies',
+    pathMatch: 'full',
   },
   {
-      path: 'about',
-      component: AboutComponent,
+    path: 'about',
+    component: AboutComponent,
   },
+
+  {
+    path: 'movies',
+    component: MovieListComponent,
+  }
+
 ];
